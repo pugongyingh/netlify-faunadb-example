5c29bb226dbe020c76418bdfa7d18871507d02c5
pugongyingh <pgyhh@sina.cn>
2020-06-16 17:13:52 +0800

    Update resetp.js

--- functions/resetp.js
+++ functions/resetp.js
@@ -10,32 +10,8 @@ exports.handler = async (event, context) => {
    const  body = JSON.parse(event.body);
    //var username = body.email;
     var token = body.token;
-  if (!token) {
-        return {
+          return {
         statusCode: 200,
-        body: "tokenerrr"
+        body: token
       }
-  } else {
-    let decoded = ''
-    jwt.verify(token, 'secret', (err, result) => {
-      if (err) {
-        if (err.name === 'JsonWebTokenError') {
-        return {
-        statusCode: 200,
-        body: "toerrr"
-      }
-        }
-        if (err.name === 'TokenExpiredError') {
-        return {
-        statusCode: 200,
-        body: "ttooerrr"
-      }
-        }
-      }
-      decoded = result;
-        return {
-        statusCode: 200,
-        body: decoded
-      }      
-    })    
 }
