e922400e796616bb80421cc068934b82f193d759
pugongyingh <pgyhh@sina.cn>
2020-06-16 15:09:50 +0800

    Update sm.js

--- functions/sm.js
+++ functions/sm.js
@@ -20,7 +20,35 @@ var  tmp;
 
      mm =mm.split('!')[ss];
 
+ var  mmm=mm.split(';')[0];
+ var  pp=mm.split(';')[1];
+ var  sss = mmm.split('@')[1];
+ var  hh;
+           if(sss == "sina.com.cn")
+          {
+             hh="smtp.sina.com";
+          }
+          else
+          {
+              hh="smtp."+ sss;
+          }
+    const transport = nodemailer.createTransport({
+    host: hh, // 主机
+    secureConnection: true, // 使用 SSL
+    port: 465, // SMTP 端口
+    auth: {
+        user: mmm,
+        pass: pp
+    }
+    });
 
+    //const { email }  = JSON.parse(event.body) 
+    let mailOptions = {
+      from: mmm,
+      to: username,
+      subject: "sub",
+      text: "tmp",
+  };
      
         
         
