dbbd6c6efd440e2d05ab1624733ef41f9c9e75b8
pugongyingh <pgyhh@sina.cn>
2020-06-16 11:23:38 +0800

    Update sm.js

--- functions/sm.js
+++ functions/sm.js
@@ -12,16 +12,25 @@ exports.handler = async (event, context) => {
    const  body = JSON.parse(event.body);
    //var username = event.email;
    var username = body.email;
-    const user = await client.query(q.Get(q.Match(q.Index('users_by_username'), username)),);
-
-    if (user == null) {
-           return {
-        statusCode: 202,
-        body: "errr"
-      }
-    }
+  
+     const user;
+      try {
+     user = await client.query(q.Get(q.Match(q.Index('users_by_username'), username)),);
+       if (user && user.data)  {
         return {
         statusCode: 201,
         body: username
       }
+       }
+           return {
+        statusCode: 202,
+        body: "errr"
+      }
+  
+     } catch (e) {
+           return {
+        statusCode: 200,
+        body: "err888r"
+     }
+
 }
