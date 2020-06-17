5cf8513a62f5f5c52e891911a5e1ebd1ccd50d8a
pugongyingh <pgyhh@sina.cn>
2020-06-17 10:03:04 +0800

    Create login.js

---
+++ functions/login.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+const faunadb = require('faunadb');
+const jwt = require('jsonwebtoken');
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: `fnADubw0wCACCJEA8UYRiSeDmSRRso8z-Wk-N5Bd`,
+});
+
+module.exports = async (req, res) => {
+    const { email, password } = req.body;
+    if ( !email || !password ) {
+        res.send('incomplete input!')
+        return;        
+    }
+    try {
+        let { data } = await client.query(q.Get(q.Match(q.Index('users_by_username'), username)),);
+        if (password !=data.password) {
+            res.send({error: 'wrong password'})
+            return
+        }
+     const token = jwt.sign(
+      {emaill: data.username},
+      'sdf8wfhh#aef2fi22',
+      {
+        expiresIn: '1h',
+      },
+    );
+    
+        res.send({ token })
+    }
+    catch (e) {
+        res.send({error: 'user not found'})
+    }
+}
