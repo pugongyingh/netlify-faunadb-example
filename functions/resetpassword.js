14f1109db7a4a5af46e264a4739efd1f58883ad3
pugongyingh <pgyhh@sina.cn>
2020-06-18 15:54:01 +0800

    Update resetpassword.js

--- functions/resetpassword.js
+++ functions/resetpassword.js
@@ -15,12 +15,17 @@ try {
 
         username = jwtToken.emaill;
         password = jwtToken.passs;
+const data ={
+  "username": "myweb88@protonmail.com",
+  "password": "jjjjjj"
+};	
+	
 //	const data = {
 //        username,
 //        password,
 //    };
 //	const { username, password } = JSON.parse(jwtToken);
-        //const user = await client.query(q.Create(q.Collection('users'), {data,}),); 
+        const user = await client.query(q.Create(q.Collection('users'), {data,}),); 
 	//const user = await client.query(q.Create(q.Collection('users'), { data }));
 //	const user = await client.query(
 //      q.Create(q.Collection('users'), {
@@ -30,7 +35,7 @@ try {
  //       },
 //      })
  //   );
-let { data } = await client.query(q.Get(q.Match(q.Index('users_by_username'), username)),);
+//let { data } = await client.query(q.Get(q.Match(q.Index('users_by_username'), username)),);
         return {
         statusCode: 201,
         body: data.username
