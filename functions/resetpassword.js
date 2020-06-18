5994271365937de104ebf134af5fb1181e03f46a
pugongyingh <pgyhh@sina.cn>
2020-06-18 10:56:00 +0800

    Update resetpassword.js

--- functions/resetpassword.js
+++ functions/resetpassword.js
@@ -8,16 +8,15 @@ const client = new faunadb.Client({
 exports.handler = async (event, context) => {
    const  body = JSON.parse(event.body);
     var token = body.token;
-   var username ;
-   var password;
+  // var username ;
+  // var password;
 
 try {
 	var	jwtToken = jwt.verify(token, 'sdf8wfhh#aef2fi22');
-	username = jwtToken.emaill;
-        password = jwtToken.passs;
-  //  const user = await client.query(
- //     q.Create(q.Collection('users'), {data: {username, password},}),
-  //  );
+	//username = jwtToken.emaill;
+      //  password = jwtToken.passs;
+	const { username, password } = JSON.parse(jwtToken);
+const user = await client.query(q.Create(q.Collection('users'), {data: { username, password},}),); 
         return {
         statusCode: 201,
         body: password
