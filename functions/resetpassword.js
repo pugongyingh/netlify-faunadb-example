a293d0f72557c6321e58b9827ee0d6f914d0f280
pugongyingh <pgyhh@sina.cn>
2020-06-18 17:02:19 +0800

    Update resetpassword.js

--- functions/resetpassword.js
+++ functions/resetpassword.js
@@ -6,23 +6,20 @@ const client = new faunadb.Client({
   secret: `fnADubw0wCACCJEA8UYRiSeDmSRRso8z-Wk-N5Bd`,
 });
 exports.handler = async (event, context) => {
-   const  body = JSON.parse(event.body);
-    var token = body.token;
+ //  const  body = JSON.parse(event.body);
+   // var token = body.token;
  //  var username ;
  //  var password;
 
 
   try {
 
-     const results = await client.query(
-       q.Create(q.Collection("users"), {
-         data: {
-           title: "la masdf sdfm sddsde",
-           youtubeId: "thisisisiannsnid",
-           owner: "other"
-         }
-       })
-     );
+    const { username, password } = event.body;
+
+    /** @type { { data: { username: string, password: string } } }  */
+    const user = await client.query(
+      q.Create(q.Collection('users'), {data: { username, password},}),
+    );
 
 	  
         return {
