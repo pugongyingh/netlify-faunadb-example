2383f38bc467f2280f52bb09b9fc76c100aa03f4
pugongyingh <pgyhh@sina.cn>
2020-06-18 17:43:52 +0800

    Update resetpassword.js

--- functions/resetpassword.js
+++ functions/resetpassword.js
@@ -11,31 +11,16 @@ exports.handler = async (event, context) => {
     var username ="88" ;
   var password ="99";
 
-	//var	jwtToken = jwt.verify(token, 'sdf8wfhh#aef2fi22');
+	var	jwtToken = jwt.verify(token, 'sdf8wfhh#aef2fi22');
 
-  //     username = jwtToken.emaill;
-  //     password = jwtToken.passs;
+       username = jwtToken.emaill;
+       password = jwtToken.passs;
 	
 	
 	const data = {
         username,
         password,
     };
-//	const { username, password } = JSON.parse(jwtToken);
-      //  const user = await client.query(q.Create(q.Collection('users'), {data,}),); 
-	//const user = await client.query(q.Create(q.Collection('users'), { data }));
-//	const user = await client.query(
-//      q.Create(q.Collection('users'), {
-//        data: {
-//  username: "myweb88@protonmail.com",
-//  password: "jjjjjj"
- //       },
- //     })
- //   );
-//let { data } = await client.query(q.Get(q.Match(q.Index('users_by_username'), username)),);
-  
-  
-  //const data = JSON.parse(event.body);
   const record = {
     data: data,
   };
