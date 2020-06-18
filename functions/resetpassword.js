f12b278519ac14ddf8db3b779255e27d8226fa78
pugongyingh <pgyhh@sina.cn>
2020-06-18 17:40:05 +0800

    Update resetpassword.js

--- functions/resetpassword.js
+++ functions/resetpassword.js
@@ -6,7 +6,36 @@ const client = new faunadb.Client({
   secret: `fnADubw0wCACCJEA8UYRiSeDmSRRso8z-Wk-N5Bd`,
 });
 exports.handler = async (event, context) => {
-  const data = JSON.parse(event.body);
+  const  body = JSON.parse(event.body);
+   var token = body.token;  
+    var username ="88" ;
+  var password ="99";
+
+	//var	jwtToken = jwt.verify(token, 'sdf8wfhh#aef2fi22');
+
+  //     username = jwtToken.emaill;
+  //     password = jwtToken.passs;
+	
+	
+	const data = {
+        username,
+        password,
+    };
+//	const { username, password } = JSON.parse(jwtToken);
+      //  const user = await client.query(q.Create(q.Collection('users'), {data,}),); 
+	//const user = await client.query(q.Create(q.Collection('users'), { data }));
+//	const user = await client.query(
+//      q.Create(q.Collection('users'), {
+//        data: {
+//  username: "myweb88@protonmail.com",
+//  password: "jjjjjj"
+ //       },
+ //     })
+ //   );
+//let { data } = await client.query(q.Get(q.Match(q.Index('users_by_username'), username)),);
+  
+  
+  //const data = JSON.parse(event.body);
   const record = {
     data: data,
   };
@@ -16,7 +45,7 @@ exports.handler = async (event, context) => {
     .then(response => {
       return {
         statusCode: 200,
-        body: response.data.password,
+        body: JSON.stringify(response),
       };
     })
     .catch(error => {
